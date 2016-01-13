0c653d0efd514c2187494900f10f4e424f5e0f9a
yherson <yherson_fsf@outlook.com>
2016-01-13 12:18:37 -0500

    Completada la funcionalidad 'Agregar <li>'

--- Prepro/scripts/script.js
+++ Prepro/scripts/script.js
@@ -2,9 +2,46 @@
 document.addEventListener('DOMContentLoaded', function(){
 
    // Declaracion de Variables
-   var mensaje = "Hola Mundo...! :D";
+   var $btnAdd = document.querySelector('.btnAdd');
+   var $ulList = document.querySelector('.Listado__ul');
+   var $inputText;
+   
 
-   // Instrucciones
-   console.log(mensaje);
-   // alert(mensaje);
+   // Click del btnAdd
+   $btnAdd.addEventListener("click", agregarLi);
+
+
+   function agregarLi(){
+      // Capturar el texto del input
+      $inputText = document.querySelector('.inputText');
+
+      // escribir dentro de un <li>
+      $ulList.innerHTML = $ulList.innerHTML + '<li>' + $inputText.value + '</li>';
+
+      // Agregar click a los li
+      agregarTodosLi();
+
+      // limpiar el input
+      $inputText.value = "";
+
+      // focalizar en el inputs
+      $inputText.focus();
+   }
+
+
+   function agregarTodosLi(){
+      var tmp = document.querySelectorAll('.Listado__ul > li');
+      var tmpLength = tmp.length - 1;
+
+      for (var i = 0; i <= tmpLength; i++) {
+         agregarCLick(tmp[i]);
+      }
+   };
+
+
+   function agregarCLick(el){
+      el.addEventListener("click",function(){
+         el.classList.toggle('tachado');
+      });
+   };
 });
--- Public/js/script.js
+++ Public/js/script.js
@@ -1,1 +1,48 @@
-document.addEventListener("DOMContentLoaded",function(){var n="Hola Mundo...! :D";console.log(n)});
+// Evento que se ejecuta una ves que el DOM este cargado
+document.addEventListener('DOMContentLoaded', function(){
+
+   // Declaracion de Variables
+   var $btnAdd = document.querySelector('.btnAdd');
+   var $ulList = document.querySelector('.Listado__ul');
+   var $inputText;
+   
+
+   // Click del btnAdd
+   $btnAdd.addEventListener("click", agregarLi);
+
+
+   function agregarLi(){
+      // Capturar el texto del input
+      $inputText = document.querySelector('.inputText');
+
+      // escribir dentro de un <li>
+      $ulList.innerHTML = $ulList.innerHTML + '<li>' + $inputText.value + '</li>';
+
+      // Agregar click a los li
+      agregarTodosLi();
+
+      // limpiar el input
+      $inputText.value = "";
+
+      // focalizar en el inputs
+      $inputText.focus();
+   }
+
+
+   function agregarTodosLi(){
+      var tmp = document.querySelectorAll('.Listado__ul > li');
+      var tmpLength = tmp.length - 1;
+
+      for (var i = 0; i <= tmpLength; i++) {
+         agregarCLick(tmp[i]);
+      }
+   };
+
+
+   function agregarCLick(el){
+      el.addEventListener("click",function(){
+         el.classList.toggle('tachado');
+      });
+   };
+});
+
