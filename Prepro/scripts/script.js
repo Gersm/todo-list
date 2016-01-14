7079463abf84a4f1c9af986bc20f4cff2742b048
yherson <yherson_fsf@outlook.com>
2016-01-14 10:55:12 -0500

    Agregado de Ids

--- Prepro/scripts/script.js
+++ Prepro/scripts/script.js
@@ -2,13 +2,13 @@
 document.addEventListener('DOMContentLoaded', function(){
 
    // Declaracion de Variables
-   var $btnAdd, $ulList, $inputText;
+   var $btnAdd = document.querySelector('#btnAdd'), $ulList, $inputText;
+   var $btnAll = document.querySelector('#btnAll'), $ulList, $inputText;
 
    actualizarVariables();
    function actualizarVariables(){
-      $btnAdd = document.querySelector('.btnAdd');
-      $ulList = document.querySelector('.Listado__ul');
-      $inputText = document.querySelector('.inputText');
+      $ulList = document.querySelector('#Listado__ul');
+      $inputText = document.querySelector('#inputText');
    }
 
    // Click al btnAdd
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function(){
       actualizarVariables();
 
       // escribir dentro de un <li>
-      $ulList.innerHTML = $ulList.innerHTML + '<li>' + $inputText.value + '</li>';
+      $ulList.innerHTML = $ulList.innerHTML + '<li class="activo">' + $inputText.value + '</li>';
 
       // Agregar click a los li
       agregarTodosLi();
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
       function agregarTodosLi(){
-         var tmp = document.querySelectorAll('.Listado__ul > li');
+         var tmp = document.querySelectorAll('#Listado__ul > li');
          var tmpLength = tmp.length - 1;
 
          for (var i = 0; i <= tmpLength; i++) {
@@ -51,35 +51,48 @@ document.addEventListener('DOMContentLoaded', function(){
       function agregarCLick(el){
          el.addEventListener("click",function(){
             el.classList.toggle('tachado');
+            el.classList.toggle('activo');
          });
       };
    };
 
+   $btnAll.addEventListener("click", filtrarLi);
 
-   // function filtrarLi(){
-   //    var $lis;
-   //    var $lisJson = {
-   //       "activo" : [],
-   //       "tachado" : []
-   //    };
-   //
-   //
-   //    // Leer todos los <li>
-   //    leerLi();
-   //
-   //    // Guardar los <li> en formato Json
-   //    guardarLi();
-   //
-   //    // Limpiar el <ul>
-   //    limpiarUl();
-   //
-   //    // Agregar los <li> correspondientes
-   //    agregarLi();
-   //
-   //
-   //    function leerLi(){
-   //       // actualizarVariables();
-   //       $lis = document.querySelectorAll('.Listado__ul > li');
-   //    }
-   // }
+   function filtrarLi(){
+      var $lis;
+      var $lisCant;
+      var $lisJson = {
+         "activo" : [],
+         "tachado" : []
+      };
+
+
+      // Leer todos los <li>
+      leerLi();
+
+      // // Guardar los <li> en formato Json
+      // guardarLi();
+      //
+      // // Limpiar el <ul>
+      // limpiarUl();
+      //
+      // // Agregar los <li> correspondientes
+      // agregarLi();
+
+
+      function leerLi(){
+         // actualizarVariables();
+         $lis = document.querySelectorAll('#Listado__ul > li');
+         $lisCant = $lis.length - 1;
+
+         for (var i = 0; i <= $lisCant; i++) {
+            // if ($lis[i].getElementsByClassName('className')) {
+            //
+            // }
+
+            $lisJson['activo'][i] = $lis[i].innerText;
+         }
+         console.log($lisJson);
+      }
+   }
 });
